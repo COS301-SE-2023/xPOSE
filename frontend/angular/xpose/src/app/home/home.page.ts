5dc86579e7db2ca828b59a983f5414042cdb8945
Mduduzi Sibiya <mdundoro@gmail.com>
2023-05-23 01:29:48 +0200

    HOME PAGE ADDED

--- frontend/angular/xpose/src/app/home/home.page.ts
+++ frontend/angular/xpose/src/app/home/home.page.ts
@@ -2,11 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss']
 })
+
 export class HomePage {
+  events: any[] = [];
 
-  constructor() {}
-
+  constructor() {
+    // Generate random events
+    for (let i = 0; i < 10; i++) {
+      const event = {
+        title: `Event ${i + 1}`,
+        date: new Date().toLocaleDateString(),
+        location: `Location ${i + 1}`
+      };
+      this.events.push(event);
+    }
+  }
 }
