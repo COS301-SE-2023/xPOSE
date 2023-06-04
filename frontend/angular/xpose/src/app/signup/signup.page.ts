3a891db92b7d94418170d7fa6a89a714c15c9cef
Sovaria Naidu <sovaria10@gmail.com>
2023-06-04 19:33:39 +0200

    updated signup page

--- frontend/angular/xpose/src/app/signup/signup.page.ts
+++ frontend/angular/xpose/src/app/signup/signup.page.ts
@@ -8,31 +8,8 @@ import { HttpClient } from '@angular/common/http';
 })
 export class SignupPage implements OnInit {
 
-  name!: string;
-  email!: string;
-  password!: string;
-
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  onSubmit() {
-    const formData = {
-      name: this.name,
-      email: this.email,
-      password: this.password
-    };
-
-    this.http.post('https://example.com/api/signup', formData).subscribe(
-      response => {
-        console.log('API response:', response);
-        // Handle successful API response
-      },
-      error => {
-        console.error('API error:', error);
-        // Handle API error
-      }
-    );
   }
 }
