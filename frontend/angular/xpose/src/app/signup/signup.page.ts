7c69f4e09e35a13e61609f108ef1838e49976234
Mduduzi Sibiya <mdundoro@gmail.com>
2023-06-05 15:46:15 +0200

    I am still working on it

--- frontend/angular/xpose/src/app/signup/signup.page.ts
+++ frontend/angular/xpose/src/app/signup/signup.page.ts
@@ -5,33 +5,13 @@ import { HttpClient } from "@angular/common/http";
 	selector: "app-signup",
 	templateUrl: "./signup.page.html",
 	styleUrls: ["./signup.page.scss"],
-})
+	})
 export class SignupPage implements OnInit {
 
-  constructor() { }
+	constructor() { /* TODO document why this constructor is empty */  }
 
-  ngOnInit() {
-  }
-<<<<<<< HEAD
-=======
-
-  onSubmit() {
-  	const formData = {
-  		name: this.name,
-  		email: this.email,
-  		password: this.password
-  	};
-
-  	this.http.post("https://example.com/api/signup", formData).subscribe(
-  		response => {
-  			console.log("API response:", response);
-  			// Handle successful API response
-  		},
-  		error => {
-  			console.error("API error:", error);
-  			// Handle API error
-  		}
-  	);
-  }
->>>>>>> c286ff92a86e9a7a2a62d9fafa39fac53c3a43b9
+	ngOnInit() {
+		// TODO document why this method 'ngOnInit' is empty
+  
+	}
 }
