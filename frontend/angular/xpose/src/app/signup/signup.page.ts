7cd163cb939111f30cb85ddb9b7bd1f1fd1bf424
Sovaria Naidu <sovaria10@gmail.com>
2023-06-05 21:48:04 +0200

    added header component

--- frontend/angular/xpose/src/app/signup/signup.page.ts
+++ frontend/angular/xpose/src/app/signup/signup.page.ts
@@ -12,26 +12,4 @@ export class SignupPage implements OnInit {
 
   ngOnInit() {
   }
-<<<<<<< HEAD
-=======
-
-  onSubmit() {
-  	const formData = {
-  		name: this.name,
-  		email: this.email,
-  		password: this.password
-  	};
-
-  	this.http.post("https://example.com/api/signup", formData).subscribe(
-  		response => {
-  			console.log("API response:", response);
-  			// Handle successful API response
-  		},
-  		error => {
-  			console.error("API error:", error);
-  			// Handle API error
-  		}
-  	);
-  }
->>>>>>> c286ff92a86e9a7a2a62d9fafa39fac53c3a43b9
 }
