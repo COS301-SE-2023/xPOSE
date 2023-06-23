ef6aba6e7698be5681ea6ae6384dd3cc9900e415
Emito Khoza <emito.khoza@gmail.com>
2023-06-23 23:24:25 +0200

    Fixed microservice

---
+++ backend/events-service/app/dist/index.d.ts
@@ -0,0 +1,3 @@
+import * as functions from 'firebase-functions';
+export declare const createNestServer: (expressInstance: any) => Promise<import("@nestjs/common").INestApplication<any>>;
+export declare const api: functions.HttpsFunction;
---
+++ backend/events-service/app/dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.api = exports.createNestServer = void 0;
+const core_1 = require("@nestjs/core");
+const platform_express_1 = require("@nestjs/platform-express");
+const app_module_1 = require("./app.module");
+const functions = require("firebase-functions");
+const express = require("express");
+const server = express();
+const createNestServer = async (expressInstance) => {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, new platform_express_1.ExpressAdapter(expressInstance));
+    return app.init();
+};
+exports.createNestServer = createNestServer;
+(0, exports.createNestServer)(server)
+    .then((v) => console.log('Nest Ready'))
+    .catch((err) => console.error('Nest broken', err));
+exports.api = functions.https.onRequest(server);
+//# sourceMappingURL=index.js.map
