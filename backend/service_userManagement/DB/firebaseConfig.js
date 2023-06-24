d7982fb7e7f94e41b719be065134031f02f0575d
Ibango7 <bangoisrael@gmail.com>
2023-06-24 21:47:32 +0200

    :construction: refactoring

--- backend/service_userManagement/DB/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("./credentials/serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://xpose-4f48c-default-rtdb.firebaseio.com"
-});
--- backend/service_userManagement/controllers/users.js
+++ backend/service_userManagement/controllers/users.js
@@ -1,8 +1,27 @@
 import { v4 as uuidv4 } from 'uuid';
+import admin from "firebase-admin";
+
 
 let users = [];
 
-export const getUsers = (req, res) => {
+export const getUsers = async (req, res) => {
+    try {
+        const db = admin.firestore();
+        const userRef = db.collection("Users");
+        const querySnapshot = await userRef.get();
+
+        querySnapshot.forEach((user) =>{
+            users.push(user.data());
+        });
+
+        res.json(users);
+
+    } catch(error){
+        console.error('Error retrieving users: ', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+
+
     res.send(users);
 }
 
