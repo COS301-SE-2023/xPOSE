490a0830a879c815ac813109767cbdec614acf75
Ibango7 <bangoisrael@gmail.com>
2023-06-25 03:16:23 +0200

    :construction: more API functionalities

--- backend/service_userManagement/controllers/users.js
+++ backend/service_userManagement/controllers/users.js
@@ -160,7 +160,7 @@ export const sendFriendRequest = async (req, res) => {
         };
 
         // Add the friend request to the recipient's FriendRequests collection
-        await admin.firestore().collection('Users').doc(requestId).collection('FriendRequests').add(friendRequest);
+        await admin.firestore().collection('Users').doc(requestId).collection('FriendRequests').doc(userId).set(friendRequest);
         res.status(200).json({ message: `Friend request sent successfully to user with id ${requestId}` });
     }catch(error){
         console.error('Error sending friend request:', error);
@@ -173,10 +173,69 @@ export const removeFriend = (req, res) =>{
     res.status(200).json({ message: `Friend with id ${requestId} removed`});
 }
 
-export const acceptFriendRequest = (req, res) =>{
-    const {requestId} = req.params;
+export const acceptFriendRequest = async (req, res) =>{
+    try{
+        const {userId, requestId} = req.params;
+        
+        // Get the recipient user document
+        const recipientDoc = await admin.firestore().collection('Users').doc(requestId).get();
 
-    res.status(200).json({ message:`Friend with ${requestId} accepted`});
+        // Check if the recipient exists
+        if (!recipientDoc.exists) {
+            return res.status(404).json({ error: 'Recipient not found' });
+        }
+
+         // Check if the friend request exists and retrieve the request document
+        const requestSnapshot = await admin
+        .firestore()
+        .collection('Users')
+        .doc(requestId)
+        .collection('FriendRequests')
+        .doc(userId)
+        .get();
+
+        // Check if the friend request document exists
+        if (!requestSnapshot.exists) {
+            return res.status(404).json({ error: 'Friend request not found' });
+        }
+
+        // Update the status of the friend request to 'accepted'
+        await admin
+            .firestore()
+            .collection('Users')
+            .doc(requestId)
+            .collection('FriendRequests')
+            .doc(userId)
+            .update({
+            status: 'accepted',
+        });
+
+        // Both recipient and sender are now friends Add friend to list of friends
+
+        // Add the new friendID to the recipient's friendIds array
+        await admin
+            .firestore()
+            .collection('Users')
+            .doc(requestId)
+            .update({
+            friendIds: admin.firestore.FieldValue.arrayUnion(userId),
+        });
+
+         // Add  new friendID to the sender's friendIds array
+        await admin
+            .firestore()
+            .collection('Users')
+            .doc(userId)
+            .update({
+            friendIds: admin.firestore.FieldValue.arrayUnion(requestId),
+        });
+
+
+        res.status(200).json({ message:`Friend with ${requestId} accepted friend request`});
+    } catch(errror){
+        console.error('Error accepting friend request:', error);
+        res.status(500).json({ error: 'An error occurred while acepting friend request' });  
+    }
 }
 
 export const rejectFriendRequest = (req, res) =>{
