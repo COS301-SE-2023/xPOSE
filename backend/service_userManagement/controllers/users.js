e8c3e1feba013e5b19190e163114522f16ded579
Ibango7 <bangoisrael@gmail.com>
2023-06-25 03:48:26 +0200

    :construction: more api functionality added

--- backend/service_userManagement/controllers/users.js
+++ backend/service_userManagement/controllers/users.js
@@ -129,13 +129,86 @@ export const deleteUser = async (req, res) => {
     }
 }
 
-export const getFriends = (req, res) =>{
-    res.send("List of friends!");
+export const getFriends = async (req, res) => {
+    try {
+        const { userId } = req.params;
+    
+        // Get the user document
+        const userDoc = await admin.firestore().collection('Users').doc(userId).get();
+    
+        // Check if the user exists
+        if (!userDoc.exists) {
+          return res.status(404).json({ error: 'User not found' });
+        }
+    
+        // Retrieve the friendIds array from the user document
+        const { friendIds } = userDoc.data();
+    
+        // Check if the friendIds array is empty
+        if (!friendIds || friendIds.length === 0) {
+          return res.status(200).json({ friends: [] });
+        }
+    
+        // Get the friend documents based on the friendIds
+        const friendDocs = await admin
+          .firestore()
+          .collection('Users')
+          .where(admin.firestore.FieldPath.documentId(), 'in', friendIds)
+          .get();
+    
+        // Extract the friend data from the friend documents
+        const friends = friendDocs.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+    
+        res.status(200).json({ friends });
+      } catch (error) {
+        console.error('Error getting friends:', error);
+        res.status(500).json({ error: 'An error occurred while getting friends' });
+      }
 }
 
-export const getFriend = (req, res) => {
-    const { requestId } = req.params 
-    res.send(`specified friend id ${requestId}`);
+export const getFriend = async (req, res) => {
+    try {
+        const { userId, requestId } = req.params;
+    
+        // Get the user document
+        const userDoc = await admin.firestore().collection('Users').doc(userId).get();
+    
+        // Check if the user exists
+        if (!userDoc.exists) {
+          return res.status(404).json({ error: 'User not found' });
+        }
+    
+        // Retrieve the friendIds array from the user document
+        const { friendIds } = userDoc.data();
+    
+        // Check if the friendIds array is empty
+        if (!friendIds || friendIds.length === 0) {
+          return res.status(404).json({ error: 'No friends found' });
+        }
+    
+        // Check if the specified friend ID exists in the friendIds array
+        if (!friendIds.includes(requestId)) {
+          return res.status(404).json({ error: `Friend with ID ${requestId} not found` });
+        }
+
+        // Get the friend document based on the friend ID
+        const friendDoc = await admin.firestore().collection('Users').doc(requestId).get();
+
+         // Check if the friend document exists
+        if (!friendDoc.exists) {
+            return res.status(404).json({ error: 'Friend document not found' });
+        }
+  
+        const friendData = friendDoc.data();
+        res.status(200).json({ friend: friendData });
+        
+      } catch (error) {
+        console.error('Error getting friend:', error);
+        res.status(500).json({ error: 'An error occurred while getting friend' });
+      }
 }
 
 export const sendFriendRequest = async (req, res) => {
