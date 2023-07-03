7415d96ce40e9ac264f1fc41141ba2c5b4bcc21a
Mduduzi Sibiya <mdundoro@gmail.com>
2023-07-03 23:57:38 +0200

    progress

--- frontend/angular/xpose/src/app/header/header.page.spec.ts
+++ frontend/angular/xpose/src/app/header/header.page.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import { HeaderPage } from './header.page';
 
 describe('HeaderPage', () => {
--- frontend/angular/xpose/src/app/post-details/post-details.page.spec.ts
+++ frontend/angular/xpose/src/app/post-details/post-details.page.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import { PostDetailsPage } from './post-details.page';
 
 describe('PostDetailsPage', () => {
