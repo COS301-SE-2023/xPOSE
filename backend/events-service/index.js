20bbf776a86f2b19d717fc40204e272b4017272b
Emito Khoza <emito.khoza@gmail.com>
2023-07-13 16:15:52 +0200

    Updated structure

--- backend/events-service/index.js
+++ backend/events-service/index.js
@@ -1,150 +1,10 @@
 const express = require('express');
-const Sequelize = require('sequelize');
+const {sequelize} = require('./data-access/sequelize');
 const { Op } = require('sequelize');
 const uploadImageToFirebase = require('./data-access/firebase.repository');
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 
-require('dotenv').config();
-
-const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
-  host: process.env.DATABASE_HOST,
-  dialect: 'mysql',
-  port: process.env.DATABASE_PORT
-});
-
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  uid: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-});
-
-const Event = sequelize.define('event', {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  title: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  latitude: {
-    type: Sequelize.FLOAT,
-    allowNull: false,
-  },
-  longitude: {
-    type: Sequelize.FLOAT,
-    allowNull: false,
-  },
-  image_url: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  privacy_setting: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  code: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-  timestamp: {
-    type: Sequelize.DATE,
-    allowNull: false,
-  },
-});
-
-const EventParticipant = sequelize.define('eventParticipant', {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  timestamp: {
-    type: Sequelize.DATE,
-    allowNull: false,
-  },
-});
-
-const EventInvitation = sequelize.define('eventInvitation', {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  response: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  timestamp: {
-    type: Sequelize.DATE,
-    allowNull: false,
-  },
-});
-
-const EventJoinRequest = sequelize.define('eventJoinRequest', {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  response: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  timestamp: {
-    type: Sequelize.DATE,
-    allowNull: false,
-  },
-});
-
-// Associations
-User.hasMany(Event, { foreignKey: 'owner_id_fk' });
-Event.belongsTo(User, { foreignKey: 'owner_id_fk' });
-
-User.belongsToMany(Event, {
-  through: EventParticipant,
-  foreignKey: 'user_id_fk',
-  otherKey: 'event_id_fk',
-});
-Event.belongsToMany(User, {
-  through: EventParticipant,
-  foreignKey: 'event_id_fk',
-  otherKey: 'user_id_fk',
-});
-
-User.belongsToMany(Event, {
-  through: EventInvitation,
-  foreignKey: 'user_id_fk',
-  otherKey: 'event_id_fk',
-});
-Event.belongsToMany(User, {
-  through: EventInvitation,
-  foreignKey: 'event_id_fk',
-  otherKey: 'user_id_fk',
-});
-
-User.belongsToMany(Event, {
-  through: EventJoinRequest,
-  foreignKey: 'user_id_fk',
-  otherKey: 'event_id_fk',
-});
-Event.belongsToMany(User, {
-  through: EventJoinRequest,
-  foreignKey: 'event_id_fk',
-  otherKey: 'user_id_fk',
-});
 
 // Initialize Express app
 const cors = require('cors');
@@ -280,4 +140,4 @@ sequelize.sync({ force: true }) // Replace `force: true` with `force: false` in 
   })
   .catch((error) => {
     console.error('Failed to start the server:', error);
-  });
+});
