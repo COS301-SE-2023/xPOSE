7fea4a963a40cc853860997f33dba228b4d9d69c
Mduduzi Sibiya <mdundoro@gmail.com>
2023-07-13 20:19:21 +0200

    upload page in progress

--- frontend/angular/xpose/src/app/event/posts/posts.page.ts
+++ frontend/angular/xpose/src/app/event/posts/posts.page.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { NavController } from '@ionic/angular';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-posts',
@@ -8,69 +6,20 @@ import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
   styleUrls: ['./posts.page.scss'],
 })
 export class PostsPage {
-  constructor(private navCtrl: NavController, private sanitizer: DomSanitizer) {
-  }
+  url: string | ArrayBuffer | null = './assets/shapes.svg';
 
+  onFileSelected(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const file = inputElement.files?.[0];
 
-  // This method will be called when user clicks on any image
-  //url; //Angular 8
-	url: any; //Angular 11, for stricter type
-	msg = "";
-	
-	//selectFile(event) { //Angular 8
-	// selectFile(event: any) { //Angular 11, for stricter type
-	// 	if(!event.target.files[0] || event.target.files[0].length == 0) {
-	// 		this.msg = 'You must select an image';
-	// 		return;
-	// 	}
-		
-	// 	var mimeType = event.target.files[0].type;
-		
-	// 	if (mimeType.match(/image\/*/) == null) {
-	// 		this.msg = "Only images are supported";
-	// 		return;
-	// 	}
-		
-	// 	var reader = new FileReader();
-	// 	reader.readAsDataURL(event.target.files[0]);
-		
-	// 	reader.onload = (_event) => {
-	// 		this.msg = "";
-	// 		this.url = reader.result; 
-	// 	}
-	// }
-
-  public uploadedImages: File[] = [];
-  public imageUrls: SafeUrl[] = [];
-
-  public selectFiles(event: any): void {
-    const files = event.target.files;
-    for (let i = 0; i < files.length; i++) {
-      const file = files[i];
-      this.uploadedImages.push(file);
-      this.uploadImage(file);
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = (event: ProgressEvent<FileReader>) => {
+        if (event.target?.result) {
+          this.url = event.target.result;
+        }
+      };
     }
   }
-  
-  public uploadImage(file: File): void {
-    const reader = new FileReader();
-    reader.onload = (e: any) => {
-      const imageUrl = e.target.result;
-      this.imageUrls.push(this.sanitizer.bypassSecurityTrustUrl(imageUrl));
-    };
-    reader.readAsDataURL(file);
-  }
-
-  
-
-
-// constructor(private sanitizer: DomSanitizer) {}
-
-public getSafeImageUrl(image: File): SafeUrl {
-  return this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(image));
-}
-
-  
-  
-  
 }
