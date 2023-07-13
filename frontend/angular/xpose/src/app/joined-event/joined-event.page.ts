7e57af3142213b836cbe203d4e0b043ae1550e9d
Mduduzi Sibiya <mdundoro@gmail.com>
2023-07-13 07:02:39 +0200

    working on join page

--- frontend/angular/xpose/src/app/joined-event/joined-event.page.ts
+++ frontend/angular/xpose/src/app/joined-event/joined-event.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Router } from '@angular/router';
+import { AuthService } from '../shared/services/auth.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-joined-event',
@@ -9,34 +12,78 @@ import { Router } from '@angular/router';
 export class JoinedEventPage implements OnInit {
   joinedEvents: any[] = []; // Modify the data type as per your event structure
   filteredEvents: any[] = []; // Initialize as an empty array
+  cards: any[] = []; // Initialize as an empty array
+  events: any[] = [];
   filterType: string = 'private'; // Default filter type
 
-  constructor(private router: Router) {}
+  constructor(
+    private afs: AngularFirestore,
+		public authService: AuthService,
+		private router: Router,
+		private http: HttpClient
+
+  ) {}
+
+  populateCards() {
+    if (this.events.length === 0) {
+      this.cards = []; // Empty the cards list when there are no events
+      } else {
+      this.cards = this.events.map(event => ({
+        title: event.eventName,
+        subtitle: event.eventDescription,
+        description: '' + event.eventLocation,
+        button: "Join event",
+        imageURL: event.imageUrl,
+        id: event.id,
+        // Add event listener to the button
+        buttonClick: function() {
+        // Redirect to event details page
+        window.location.href = "/event?id=" + event.id;
+        }
+      }));
+      }
+    }
 
   ngOnInit() {
-    this.loadJoinedEvents();
+    this.getEventsFromAPI();
+  }
+  getEventsFromAPI() {
+    this.http.get<Event[]>("http://localhost:8000/e/events").subscribe((events: Event[]) => {
+      console.log(events);
+		this.events = events;
+      this.populateCards();
+    });
   }
 
   loadJoinedEvents() {
     // Call your event service method to fetch joined events
-    // Example code:
-    this.joinedEvents = [
-      { id: 1, name: 'Private Event 1', type: 'private', image: 'assets/images/download.jpg' },
-      { id: 2, name: 'Private Event 2', type: 'private', image: 'assets/images/download.jpg' },
-      { id: 3, name: 'Public Event 1', type: 'public', image: 'assets/images/download.jpg' },
-      { id: 4, name: 'Public Event 2', type: 'public', image: 'assets/images/download.jpg' },
-    ];
-    this.applyFilter(); // Apply the initial filter
+    if (this.events.length === 0) {
+      this.cards = []; // Empty the cards list when there are no events
+      } else {
+      this.cards = this.events.map(event => ({
+        title: event.eventName,
+        subtitle: event.eventDescription,
+        description: '' + event.eventLocation,
+        button: "Join event",
+        imageURL: event.imageUrl,
+        id: event.id,
+        // Add event listener to the button
+        buttonClick: function() {
+        // Redirect to event details page
+        window.location.href = "/event?id=" + event.id;
+        }
+      }));
+      }
   }
-
   applyFilter() {
-    if (this.filterType === 'private') {
-      this.filteredEvents = this.joinedEvents.filter((event) => event.type === 'private');
-    } else if (this.filterType === 'public') {
-      this.filteredEvents = this.joinedEvents.filter((event) => event.type === 'public');
-    } else {
-      this.filteredEvents = this.joinedEvents; // No filter applied
-    }
+  //   if (this.filterType === 'private') {
+  //     this.filteredEvents = this.joinedEvents.filter((event) => event.type === 'private');
+  //   } else if (this.filterType === 'public') {
+  //     this.filteredEvents = this.joinedEvents.filter((event) => event.type === 'public');
+  //   } else {
+  //     this.filteredEvents = this.joinedEvents; // No filter applied
+  //   }
+  
   }
 
   onEvent() {
