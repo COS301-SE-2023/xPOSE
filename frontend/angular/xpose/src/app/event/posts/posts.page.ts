6748713c958ce0cdccd2ebc9f1c61c0261d0ec69
Mduduzi Sibiya <mdundoro@gmail.com>
2023-07-13 18:36:16 +0200

    upload page in progress

--- frontend/angular/xpose/src/app/event/posts/posts.page.ts
+++ frontend/angular/xpose/src/app/event/posts/posts.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-posts',
@@ -7,7 +8,7 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./posts.page.scss'],
 })
 export class PostsPage {
-  constructor(private navCtrl: NavController) {
+  constructor(private navCtrl: NavController, private sanitizer: DomSanitizer) {
   }
 
 
@@ -17,26 +18,59 @@ export class PostsPage {
 	msg = "";
 	
 	//selectFile(event) { //Angular 8
-	selectFile(event: any) { //Angular 11, for stricter type
-		if(!event.target.files[0] || event.target.files[0].length == 0) {
-			this.msg = 'You must select an image';
-			return;
-		}
+	// selectFile(event: any) { //Angular 11, for stricter type
+	// 	if(!event.target.files[0] || event.target.files[0].length == 0) {
+	// 		this.msg = 'You must select an image';
+	// 		return;
+	// 	}
 		
-		var mimeType = event.target.files[0].type;
+	// 	var mimeType = event.target.files[0].type;
 		
-		if (mimeType.match(/image\/*/) == null) {
-			this.msg = "Only images are supported";
-			return;
-		}
+	// 	if (mimeType.match(/image\/*/) == null) {
+	// 		this.msg = "Only images are supported";
+	// 		return;
+	// 	}
 		
-		var reader = new FileReader();
-		reader.readAsDataURL(event.target.files[0]);
+	// 	var reader = new FileReader();
+	// 	reader.readAsDataURL(event.target.files[0]);
 		
-		reader.onload = (_event) => {
-			this.msg = "";
-			this.url = reader.result; 
-		}
-	}
+	// 	reader.onload = (_event) => {
+	// 		this.msg = "";
+	// 		this.url = reader.result; 
+	// 	}
+	// }
+
+  public uploadedImages: File[] = [];
+  public imageUrls: SafeUrl[] = [];
+
+  public selectFiles(event: any): void {
+    const files = event.target.files;
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      this.uploadedImages.push(file);
+      this.uploadImage(file);
+    }
+  }
+  
+  public uploadImage(file: File): void {
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      const imageUrl = e.target.result;
+      this.imageUrls.push(this.sanitizer.bypassSecurityTrustUrl(imageUrl));
+    };
+    reader.readAsDataURL(file);
+  }
+
+  
+
+
+// constructor(private sanitizer: DomSanitizer) {}
+
+public getSafeImageUrl(image: File): SafeUrl {
+  return this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(image));
+}
+
+  
+  
   
 }
