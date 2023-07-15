e1dd703ea1d4b24be7fba77d4b614794fdd810db
Emito Khoza <emito.khoza@gmail.com>
2023-07-15 20:29:48 +0200

    Updated events delete

--- backend/events-service/index.js
+++ backend/events-service/index.js
@@ -168,22 +168,49 @@ app.put('/events/:code', upload.single('image'), async (req, res) => {
 
 // Delete an event
 app.delete('/events/:code', async (req, res) => {
-  try {
-    const event = await Event.findOne({
-        where: {
-            code: req.params.code
-    }});
+    try {
+        console.log('Deleting event:');
+        console.log(req);
+        // If there's no uid, throw an error
+        // if (!req.body.uid) {
+        //     res.status(400).json({ error: 'No uid provided' });
+        //     return;
+        // }
 
-    if (event) {
-      await event.destroy();
-      res.sendStatus(204);
-    } else {
-      res.status(404).json({ error: 'Event not found' });
+        // const user = await User.findOne({
+        //     where: {
+        //         uid: req.body.uid,
+        //     },
+        // });
+
+        // // If user doesn't exist, throw an error
+        // if (!user) {
+        //     res.status(404).json({ error: 'User does not exist' });
+        //     return;
+        // }
+
+        const event = await Event.findOne({
+            where: {
+                code: req.params.code,
+            },
+        });
+
+        if (event) {
+            // Verify if the user owns this event
+            // if (user.id !== event.owner_id_fk) {
+            //     res.status(400).json({ error: 'User does not own this event' });
+            //     return;
+            // }
+
+            await event.destroy();
+            res.sendStatus(204);
+        } else {
+            res.status(404).json({ error: 'Event not found' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to delete the event' });
     }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to delete the event' });
-  }
 });
 
 // invite user
