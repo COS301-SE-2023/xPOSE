ecde9b290d6a5d0a58f6495a39950b043a230df9
Emito Khoza <emito.khoza@gmail.com>
2023-07-16 02:47:46 +0200

    Updated event invite feature

--- backend/events-service/index.js
+++ backend/events-service/index.js
@@ -1,21 +1,32 @@
 const express = require('express');
-const { sequelize, User, Event } = require('./data-access/sequelize');
+const { sequelize, User, Event, EventInvitation } = require('./data-access/sequelize');
 const { Op } = require('sequelize');
 const uploadImageToFirebase = require('./data-access/firebase.repository');
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 const { generateUniqueCode, EventBuilder } = require('./libs/Events');
 const admin = require('firebase-admin');
-
+const bodyParser = require('body-parser');
 // Initialize Express app
 const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors());
+// app.use(bodyParser());
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
 
 // Create an event
 app.post('/events', upload.single('image'), async (req, res) => {
     try {
+        console.log('-------------------------');
+        // const eventBuilder = new EventBuilder();
+        const imageFile = req.file;
+        // let image_url = await uploadImageToFirebase(req.body.uid, imageFile);
+        console.log(imageFile);
+        console.log('-------------------------');
+        console.log(req.body);
         // if user doesn't exist, then create them if they exist in firebase
         const eventBuilder = new EventBuilder();
         let image_url = await uploadImageToFirebase(req.body.uid, req.file);
@@ -167,27 +178,27 @@ app.put('/events/:code', upload.single('image'), async (req, res) => {
   
 
 // Delete an event
-app.delete('/events/:code', async (req, res) => {
+app.post('/events/delete/:code', async (req, res) => {
     try {
         console.log('Deleting event:');
-        console.log(req);
+        console.log(req.body);
         // If there's no uid, throw an error
-        // if (!req.body.uid) {
-        //     res.status(400).json({ error: 'No uid provided' });
-        //     return;
-        // }
+        if (!req.body.uid) {
+            res.status(400).json({ error: 'No uid provided' });
+            return;
+        }
 
-        // const user = await User.findOne({
-        //     where: {
-        //         uid: req.body.uid,
-        //     },
-        // });
+        const user = await User.findOne({
+            where: {
+                uid: req.body.uid,
+            },
+        });
 
-        // // If user doesn't exist, throw an error
-        // if (!user) {
-        //     res.status(404).json({ error: 'User does not exist' });
-        //     return;
-        // }
+        // If user doesn't exist, throw an error
+        if (!user) {
+            res.status(404).json({ error: 'User does not exist' });
+            return;
+        }
 
         const event = await Event.findOne({
             where: {
@@ -197,10 +208,10 @@ app.delete('/events/:code', async (req, res) => {
 
         if (event) {
             // Verify if the user owns this event
-            // if (user.id !== event.owner_id_fk) {
-            //     res.status(400).json({ error: 'User does not own this event' });
-            //     return;
-            // }
+            if (user.id !== event.owner_id_fk) {
+                res.status(400).json({ error: 'User does not own this event' });
+                return;
+            }
 
             await event.destroy();
             res.sendStatus(204);
@@ -213,15 +224,71 @@ app.delete('/events/:code', async (req, res) => {
     }
 });
 
-// invite user
-app.post('/events/invite/:uid', async (req, res) => {
-    res.status(500).json({ error: 'Not ready'});
+// Invite user
+app.post('/events/:code/invite/', async (req, res) => {
+    try {
+        console.log(req.body);
+        const { uid } = req.body;
+        const { code } = req.params;
+
+        // Find the user with the provided uid
+        const user = await User.findOne({
+            where: {
+                uid: uid,
+            },
+        });
+
+        // If user doesn't exist, throw an error
+        if (!user) {
+            res.status(404).json({ error: 'User not found' });
+            return;
+        }
+
+        // Find the event with the provided eventId
+        const event = await Event.findOne({
+            where: {
+                code: code,
+            },
+        });
+
+        // If event doesn't exist, throw an error
+        if (!event) {
+            res.status(404).json({ error: 'Event not found' });
+            return;
+        }
+
+        // Check if the user is already invited to the event
+        const existingInvitation = await EventInvitation.findOne({
+            where: {
+                user_id_fk: user.id,
+                event_id_fk: event.id,
+            },
+        });
+
+        if (existingInvitation) {
+            res.status(400).json({ error: 'User is already invited to the event' });
+            return;
+        }
+
+        // Create a new invitation
+        const invitation = await EventInvitation.create({
+            user_id_fk: user.id,
+            event_id_fk: event.id,
+            response: 'pending',
+            timestamp: new Date(),
+        });
+
+        res.json(invitation);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to invite the user to the event' });
+    }
 });
 
 // response from invited user
 app.put('/events/invite/:code', async (req, res) => {
     res.status
-})
+});
 
 // user requesting to join
 app.post('/events/request/:uid', async (req, res) => {
