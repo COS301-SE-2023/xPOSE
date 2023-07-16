c5abdd5aaaa881ca38df008fd1818e1f8f4379b9
Emito Khoza <emito.khoza@gmail.com>
2023-07-16 14:48:37 +0200

    Updating getting of a single event

--- backend/events-service/index.js
+++ backend/events-service/index.js
@@ -188,8 +188,35 @@ app.get('/events', upload.none(), async (req, res) => {
 });
 
 // Get a single event by code
-app.get('/events/:code', async (req, res) => {
+app.get('/events/:code', upload.none(), async (req, res) => {
     try {
+        const { uid } = req.body;
+
+        if(!uid) {
+            res.status(400).json({ error: 'Missing required fields' });
+            return;
+        }
+
+        // Find the user with the provided uid
+        let user = await User.findOne({
+            where: {
+                uid: uid,
+            },
+        });
+
+        // If user doesn't exist, create them
+        if (!user) {
+            const firestoreUserDoc = await admin.firestore().collection('Users').doc(uid).get();
+
+            if (firestoreUserDoc.exists) {
+                // Create the user in the Users table
+                user = await User.create({ uid: uid });
+            } else {
+                res.status(400).json({ error: 'Invalid user' });
+                return;
+            }
+        }
+
         const event = await Event.findOne({
             where: {
                 code: req.params.code
@@ -203,6 +230,49 @@ app.get('/events/:code', async (req, res) => {
 
         if (event) {
             const { owner_id_fk, ...eventData } = event.toJSON();
+        
+          // Check if user is the owner
+          if (user.id === owner_id_fk) {
+            eventData.user_event_position = 'owner';
+          } else {
+            let participant = await EventParticipant.findOne({
+              where: {
+                user_id_fk: user.id,
+                event_id_fk: event.id,
+              },
+            });
+        
+            if (participant) {
+              eventData.user_event_position = 'participant';
+            } else {
+              let joinRequest = await EventJoinRequest.findOne({
+                where: {
+                  user_id_fk: user.id,
+                  event_id_fk: event.id,
+                  response: 'pending',
+                },
+              });
+        
+              if (joinRequest) {
+                eventData.user_event_position = 'requested';
+              } else {
+                let invitation = await EventInvitation.findOne({
+                  where: {
+                    user_id_fk: user.id,
+                    event_id_fk: event.id,
+                    response: 'pending',
+                  },
+                });
+        
+                if (invitation) {
+                  eventData.user_event_position = 'invited';
+                } else {
+                  eventData.user_event_position = 'none';
+                }
+              }
+            }
+          }
+
             eventData.owner = event.owner.uid;
             res.json(eventData);
         } else {
