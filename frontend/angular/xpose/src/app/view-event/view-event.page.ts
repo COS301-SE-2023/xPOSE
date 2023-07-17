93396890e2bf26f27989d60000997f9a1ad9ba07
Emito Khoza <emito.khoza@gmail.com>
2023-07-17 09:48:29 +0200

    Connected view event to the api and retrieved data

--- frontend/angular/xpose/src/app/view-event/view-event.page.ts
+++ frontend/angular/xpose/src/app/view-event/view-event.page.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-view-event',
@@ -9,26 +11,37 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ViewEventPage implements OnInit {
   isJoined: boolean = false; // Add the isJoined property
-  events: any;
   eventpost: any;
   event_id: any;
+  event: any;
 
   constructor(private router: Router,
               private route: ActivatedRoute,
-              private http: HttpClient
-              ) { }
+              private http: HttpClient,
+		private afAuth: AngularFireAuth
+
+            ) { }
   ngOnInit() {
     this.event_id = this.route.snapshot.paramMap.get('id');
     console.log(this.event_id);
-    // this.getEventsFromAPI();
+    this.getEventDataFromAPI();
     // this.postEventsFromAPI();
   }
+  
 
-  getEventsFromAPI() {
-    this.http.get("http://localhost:8000/e/events/").subscribe((data) => {
-      console.log(data);
-      this.events = data;
+  getEventDataFromAPI() {
+    this.getCurrentUserId().subscribe((uid) => {
+      if (uid) {
+        this.http.get(`http://localhost:8000/e/events/${this.event_id}?uid=${uid}`).subscribe((data) => {
+          this.event = data;
+          console.log(data); 
+        });
+      }
+      else {
+        console.log("no user id");
+      }
     });
+
   }
 
   postEventsFromAPI() {
@@ -46,6 +59,21 @@ export class ViewEventPage implements OnInit {
     });
   }
 
+  getCurrentUserId(): Observable<string> {
+    return this.afAuth.authState.pipe(
+      map((user) => {
+      if (user) {
+        return user.uid;
+      } else {
+        // throw error
+        // some extra stuff
+        
+        console.log('No user is currently logged in.');
+        return '';
+      }
+      })
+    );
+    }
 
   populateCards() {
     throw new Error('Method not implemented.');
