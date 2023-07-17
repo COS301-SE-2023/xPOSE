51e59b29ea22f26952d0125763e7b470be0e4bf5
Emito Khoza <emito.khoza@gmail.com>
2023-07-17 13:46:28 +0200

    Added google maps to the view page

--- frontend/angular/xpose/src/app/view-event/view-event.page.ts
+++ frontend/angular/xpose/src/app/view-event/view-event.page.ts
@@ -1,15 +1,16 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, map } from 'rxjs';
+// import { Loader } from '@googlemaps/js-api-loader';
 
 @Component({
   selector: 'app-view-event',
   templateUrl: './view-event.page.html',
   styleUrls: ['./view-event.page.scss'],
 })
-export class ViewEventPage implements OnInit {
+export class ViewEventPage implements OnInit, AfterViewInit {
   isJoined: boolean = false; // Add the isJoined property
   eventpost: any;
   event_id: any;
@@ -20,7 +21,15 @@ export class ViewEventPage implements OnInit {
               private http: HttpClient,
 		private afAuth: AngularFireAuth
 
-            ) { }
+            ) { 
+              this.map = null;
+              this.marker = null;
+
+            }
+
+  map: google.maps.Map | null;
+  marker: google.maps.Marker | null;
+
   ngOnInit() {
     this.event_id = this.route.snapshot.paramMap.get('id');
     console.log(this.event_id);
@@ -74,6 +83,38 @@ export class ViewEventPage implements OnInit {
       })
     );
     }
+
+    ngAfterViewInit() {
+      this.initMap();
+    }
+  
+    initMap() {
+      console.log('Loading map');
+      const mapContainer = document.getElementById('map');
+      console.log(`map container ${mapContainer}`);
+      
+      if (mapContainer instanceof HTMLElement) {
+        console.log(`Loading map`);
+        const mapOptions: google.maps.MapOptions = {
+          center: { lat: parseFloat(this.event.latitude), lng: parseFloat(this.event.longitude) },
+          zoom: 14,
+        };
+        
+        console.log(`Attributes showing ${this.event.latitude} ${this.event.longitude}`);
+        this.map = new google.maps.Map(mapContainer, mapOptions);
+        
+        console.log(`Map loaded`);
+        this.marker = new google.maps.Marker({
+          position: { lat: parseFloat(this.event.latitude), lng: parseFloat(this.event.longitude) },
+          map: this.map,
+          title: 'Event Location',
+        });
+        console.log(`Marker loaded`);
+      } else {
+        console.error('Map container element not found');
+      }
+    }
+    
 
   populateCards() {
     throw new Error('Method not implemented.');
