b102b45a6f869efcbe9851efcceb4a0a8b0c80be
Emito Khoza <emito.khoza@gmail.com>
2023-07-18 17:55:52 +0200

    Updated service configuration

---
+++ backend/event-chats-service/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const admin = require('firebase-admin');
+
+// Initialize Firebase Admin SDK
+const serviceAccount = require('../permissions.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://xpose-4f48c-default-rtdb.firebaseio.com',
+  storageBucket: 'gs://xpose-4f48c.appspot.com',
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
+
