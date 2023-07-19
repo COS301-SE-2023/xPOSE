f7978c57eb42cd21cf76e4a9bd9882afa72f0fa0
Mduduzi Sibiya <mdundoro@gmail.com>
2023-07-19 17:58:17 +0200

    joined-events on progress

--- frontend/angular/xpose/src/app/joined-event/joined-event.page.ts
+++ frontend/angular/xpose/src/app/joined-event/joined-event.page.ts
@@ -3,6 +3,8 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Router } from '@angular/router';
 import { AuthService } from '../shared/services/auth.service';
 import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-joined-event',
@@ -10,50 +12,87 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./joined-event.page.scss'],
 })
 export class JoinedEventPage implements OnInit {
-  joinedEvents: any[] = []; // Modify the data type as per your event structure
-  filteredEvents: any[] = []; // Initialize as an empty array
-  cards: any[] = []; // Initialize as an empty array
-  events: any[] = [];
-  filterType: string = 'private'; // Default filter type
-
-  constructor(
-    private afs: AngularFirestore,
+  loading: boolean = true;
+	searchResults: { title: string; description: string; }[] | undefined;
+	constructor(
+		private afs: AngularFirestore,
 		public authService: AuthService,
 		private router: Router,
-		private http: HttpClient
+		private http: HttpClient,
+		private afAuth: AngularFireAuth
+		) {
+	
+	   }
 
-  ) {}
-
-  populateCards() {
-    if (this.events.length === 0) {
-      this.cards = []; // Empty the cards list when there are no events
-      } else {
-      this.cards = this.events.map(event => ({
-        title: event.eventName,
-        subtitle: event.eventDescription,
-        description: '' + event.eventLocation,
-        button: "Join event",
-        imageURL: event.imageUrl,
-        id: event.id,
-        // Add event listener to the button
-        buttonClick: function() {
-        // Redirect to event details page
-        window.location.href = "/event?id=" + event.id;
-        }
-      }));
-      }
-    }
-
-  ngOnInit() {
-    this.getEventsFromAPI();
-  }
+	ngOnInit() {
+		this.getEventsFromAPI();
+	}
+	   // get events from firebase and display
+	   
   getEventsFromAPI() {
-    this.http.get<Event[]>("http://localhost:8000/e/events").subscribe((events: Event[]) => {
-      console.log(events);
-		this.events = events;
-      this.populateCards();
-    });
+
+	this.getCurrentUserId().subscribe((uid) => {
+		if(uid){
+			console.log(`We got that ${uid}`);
+			this.http.get<Event[]>(`http://localhost:8000/e/events?uid=${uid}`).subscribe((events: Event[]) => {
+				console.log(events);
+				  this.events = events;
+				this.populateCards();
+			  });
+			  this.loading = false;
+		}
+		else {
+			console.log("no user id");
+			this.loading = false;
+		}
+	});
   }
+
+  getCurrentUserId(): Observable<string> {
+	return this.afAuth.authState.pipe(
+	  map((user) => {
+		if (user) {
+		  return user.uid;
+		} else {
+			// throw error
+			// some extra stuff
+		  console.log('No user is currently logged in.');
+		  return '';
+		}
+	  })
+	);
+  }
+
+ populateCards() {
+	if (this.events.length === 0) {
+		this.cards = []; // Empty the cards list when there are no events
+	  } else {
+		this.cards = this.events.map(event => ({
+		  title: event.title,
+		  location: `(${event.latitude}, ${event.longitude})`,
+		  description: '' + event.description,
+		  button: "Join event",
+		  image_url: event.image_url,
+		  longitude: event.longitude,
+		  latitude: event.latitude,
+		  id: event.code,
+		  created_at: event.createdAt,
+		  start_date: event.start_date,
+		  end_date: event.end_date,
+		  // Add event listener to the button
+		  buttonClick: function() {
+			// Redirect to event details page
+			console.log("Redirecting to event details page: ", event.id)
+			// window.location.href = "/view-event/" + event.id;
+		  }
+		}));
+	  }
+  }
+  events: any[] = [];
+
+  cards: any[] = [
+
+  ];
 
   loadJoinedEvents() {
     // Call your event service method to fetch joined events
@@ -84,6 +123,10 @@ export class JoinedEventPage implements OnInit {
   //     this.filteredEvents = this.joinedEvents; // No filter applied
   //   }
   
+  }
+  filterType(){
+    //implement filter
+    
   }
   eventDetails() {
 		this.router.navigate(['/view-event']);
