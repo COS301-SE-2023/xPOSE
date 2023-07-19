a51bcdcb442c97b7d1d851e5cd3fd3c889efa7ca
Mduduzi Sibiya <mdundoro@gmail.com>
2023-07-19 23:56:23 +0200

    Button view event fixed

--- frontend/angular/xpose/src/app/view-event/view-event.page.ts
+++ frontend/angular/xpose/src/app/view-event/view-event.page.ts
@@ -1,10 +1,8 @@
-import { HttpClient } from '@angular/common/http';
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
-// import { Loader } from '@googlemaps/js-api-loader';
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-view-event',
@@ -12,21 +10,20 @@ import { Observable, map } from 'rxjs';
   styleUrls: ['./view-event.page.scss'],
 })
 export class ViewEventPage implements OnInit, AfterViewInit {
-  isJoined: boolean = false; // Add the isJoined property
+  isJoined: boolean = false;
   eventpost: any;
   event_id: any;
-  event: any;
+  event: any = {}; // Initialize the event object with an empty object
 
-  constructor(private router: Router,
-              private route: ActivatedRoute,
-              private http: HttpClient,
-		private afAuth: AngularFireAuth
-
-            ) { 
-              this.map = null;
-              this.marker = null;
-
-            }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private afAuth: AngularFireAuth
+  ) {
+    this.map = null;
+    this.marker = null;
+  }
 
   map: google.maps.Map | null;
   marker: google.maps.Marker | null;
@@ -35,111 +32,85 @@ export class ViewEventPage implements OnInit, AfterViewInit {
     this.event_id = this.route.snapshot.paramMap.get('id');
     console.log(this.event_id);
     this.getEventDataFromAPI();
-    // this.postEventsFromAPI();
   }
-  
 
   getEventDataFromAPI() {
     this.getCurrentUserId().subscribe((uid) => {
       if (uid) {
-        this.http.get(`http://localhost:8000/e/events/${this.event_id}?uid=${uid}`).subscribe((data) => {
-          this.event = data;
-          console.log(data); 
-        });
+        this.http
+          .get(`http://localhost:8000/e/events/${this.event_id}?uid=${uid}`)
+          .subscribe((data) => {
+            this.event = data;
+            console.log(data);
+            // Call initMap() after the event object has been fetched and contains the required properties.
+            this.initMap();
+          });
+      } else {
+        console.log('no user id');
       }
-      else {
-        console.log("no user id");
-      }
-    });
-
-  }
-
-  postEventsFromAPI() {
-    let body = {
-      eventCode:"2EFXE7",
-        eventCreator:"yb6gGaZNj9SKGsYKrCkeDPVA5JA3",
-        eventDescription:"Venue: Sheraton Hotel",
-        eventEndDate: {_seconds: 1689292800, _nanoseconds: 0},
-
-            }
-
-    this.http.post("http://localhost:8000/e/events/", body).subscribe((data) => {
-      console.log(data);
-      this.eventpost = data;
     });
   }
 
   getCurrentUserId(): Observable<string> {
     return this.afAuth.authState.pipe(
       map((user) => {
-      if (user) {
-        return user.uid;
-      } else {
-        // throw error
-        // some extra stuff
-        
-        console.log('No user is currently logged in.');
-        return '';
-      }
+        if (user) {
+          return user.uid;
+        } else {
+          console.log('No user is currently logged in.');
+          return '';
+        }
       })
     );
-    }
+  }
 
-    ngAfterViewInit() {
-      this.initMap();
-    }
-  
-    initAutcomplete() {
-      const input = document.getElementById('locationInput') as HTMLInputElement;
-      const autocomplete = new google.maps.places.Autocomplete(input);
-      
-    }
+  ngAfterViewInit() {
+    // Don't call initMap() here, it should be called after the event data has been fetched in getEventDataFromAPI().
+    // this.initMap();
+  }
 
-    initMap() {
-      console.log('Loading map');
-      const mapContainer = document.getElementById('map');
-      console.log(`map container ${mapContainer}`);
-      
-      if (mapContainer instanceof HTMLElement) {
-        console.log(`Loading map`);
-        const mapOptions: google.maps.MapOptions = {
-          center: { lat: parseFloat(this.event.latitude), lng: parseFloat(this.event.longitude) },
-          zoom: 14,
-        };
-        
-        console.log(`Attributes showing ${this.event.latitude} ${this.event.longitude}`);
-        this.map = new google.maps.Map(mapContainer, mapOptions);
-        
-        console.log(`Map loaded`);
-        this.marker = new google.maps.Marker({
-          position: { lat: parseFloat(this.event.latitude), lng: parseFloat(this.event.longitude) },
-          map: this.map,
-          title: 'Event Location',
-        });
-        console.log(`Marker loaded`);
-      } else {
-        console.error('Map container element not found');
-      }
-    }
-    
+  initMap() {
+    console.log('Loading map');
+    const mapContainer = document.getElementById('map');
+    console.log(`map container ${mapContainer}`);
 
-  populateCards() {
-    throw new Error('Method not implemented.');
+    if (mapContainer instanceof HTMLElement) {
+      console.log(`Loading map`);
+      const mapOptions: google.maps.MapOptions = {
+        center: {
+          lat: parseFloat(this.event?.latitude),
+          lng: parseFloat(this.event?.longitude),
+        },
+        zoom: 14,
+      };
+
+      console.log(
+        `Attributes showing ${this.event?.latitude} ${this.event?.longitude}`
+      );
+      this.map = new google.maps.Map(mapContainer, mapOptions);
+
+      console.log(`Map loaded`);
+      this.marker = new google.maps.Marker({
+        position: {
+          lat: parseFloat(this.event?.latitude),
+          lng: parseFloat(this.event?.longitude),
+        },
+        map: this.map,
+        title: 'Event Location',
+      });
+      console.log(`Marker loaded`);
+    } else {
+      console.error('Map container element not found');
+    }
   }
 
   joinAndRedirect() {
-    const joinButton = document.getElementById('join-button');
-    if (joinButton?.classList.contains('joined')) {
+    if (this.isJoined) {
       return;
     }
 
-    if (joinButton) {
-      joinButton.innerText = 'Joined';
-      joinButton.classList.add('joined');
-    }
+    // Perform join event logic here
 
-    // Redirect to the home page
-    this.router.navigate(['/home']);
+    this.isJoined = true;
   }
-
 }
