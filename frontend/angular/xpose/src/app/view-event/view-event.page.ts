14a8c2d58f26a72c455b3af10ff79ff2e6d55ce0
Mduduzi Sibiya <mdundoro@gmail.com>
2023-07-21 01:12:23 +0200

    All pages footer update

--- frontend/angular/xpose/src/app/view-event/view-event.page.ts
+++ frontend/angular/xpose/src/app/view-event/view-event.page.ts
@@ -289,4 +289,30 @@ export class ViewEventPage implements OnInit, AfterViewInit {
     this.event.participants.push(participant);
   }
   
+
+  //////////////////footer//////////////////////
+  search() {
+		this.router.navigateByUrl('/search');
+	}
+	viewEvent() {
+		this.router.navigate(['/event']);
+	}
+	eventDetails(event_id: string) {
+		this.router.navigate(['/view-event', event_id]);
+	}
+	onEvent(){
+		this.router.navigate(['/create-event']);
+	}
+	onNotifications(){
+		this.router.navigate(['/notification']);
+	}
+	onProfile(){
+		this.router.navigate(['/profile']);
+	}  
+	onJoinedEvent(){
+		this.router.navigate(['/joined-event']);
+	}
+	onHome(){
+		this.router.navigate(['/home']);
+	}
 }
