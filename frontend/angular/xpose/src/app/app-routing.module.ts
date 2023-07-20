939f34a33ff0141d3d6e5c5d285c5530e6ea8279
Mduduzi Sibiya <mdundoro@gmail.com>
2023-07-20 15:00:28 +0200

    Search page generated

--- frontend/angular/xpose/src/app/app-routing.module.ts
+++ frontend/angular/xpose/src/app/app-routing.module.ts
@@ -69,7 +69,11 @@ const routes: Routes = [
   {
     path: 'edit',
     loadChildren: () => import('./edit/edit.module').then( m => m.EditPageModule)
+  },  {
+    path: 'search',
+    loadChildren: () => import('./search/search.module').then( m => m.SearchPageModule)
   },
+
 
 
 ];
