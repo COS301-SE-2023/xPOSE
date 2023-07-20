3a269d90e7bcafb35795790b1517223b045a1fa0
Mduduzi Sibiya <mdundoro@gmail.com>
2023-07-20 17:33:42 +0200

    Event gallery updated

--- frontend/angular/xpose/src/app/event/posts/posts.page.ts
+++ frontend/angular/xpose/src/app/event/posts/posts.page.ts
@@ -15,15 +15,15 @@ interface Item {
 export class PostsPage {
   data: Item[] = [
     { imageSrc: '../assets/images/download.jpg', imageAlt: '1' },
-    { imageSrc: '../assets/images/youth.jpg', imageAlt: '2' },
+    { imageSrc: '../assets/images/qrcode.png', imageAlt: '2' },
     { imageSrc: '../assets/images/images.jpg', imageAlt: '3' },
     { imageSrc: '../assets/images/qrcode.png', imageAlt: '4' },
-    { imageSrc: '../assets/images/image1.webp', imageAlt: '5' },
-    { imageSrc: '../assets/images/image2.webp', imageAlt: '6' },{ imageSrc: '../assets/images/download.jpg', imageAlt: '1' },
+    { imageSrc: '../assets/images/image2.webp', imageAlt: '5' },
+    { imageSrc: '../assets/images/image1.webp', imageAlt: '6' },{ imageSrc: '../assets/images/download.jpg', imageAlt: '1' },
     { imageSrc: '../assets/images/youth.jpg', imageAlt: '7' },
     { imageSrc: '../assets/images/images.jpg', imageAlt: '8' },
-    { imageSrc: '../assets/images/qrcode.png', imageAlt: '9' },
-    { imageSrc: '../assets/images/image1.webp', imageAlt: '10' },
+    { imageSrc: '../assets/images/youth.png', imageAlt: '9' },
+    { imageSrc: '../assets/images/qrcode.png', imageAlt: '10' },
     { imageSrc: '../assets/images/image2.webp', imageAlt: '11' },
     
     // Add more items as needed...
@@ -32,6 +32,5 @@ export class PostsPage {
   constructor(private galleryDataService: GalleryDataService) {
     this.galleryDataService.setData(this.data);
   }
-
   // Other component logic goes here.
 }
