df6ab79d7f349e8ab263d1e976818bf2ebc9e74f
Sovaria Naidu <sovaria10@gmail.com>
2023-07-21 00:47:12 +0200

    updated user-profile.html

--- frontend/angular/xpose/src/app/user-profile/user-profile.page.ts
+++ frontend/angular/xpose/src/app/user-profile/user-profile.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { AuthService } from "../shared/services/auth.service";
 import { Service } from '../service/service';
 
 @Component({
@@ -8,46 +9,23 @@ import { Service } from '../service/service';
   styleUrls: ['./user-profile.page.scss'],
 })
 export class UserProfilePage implements OnInit {
-  user: any;
-  areFriends: boolean;
+  user: {
+    photoURL: string;
+    // displayName: string;
+  };
 
   constructor(
-    private route: ActivatedRoute,
-    private service: Service
-  ) {}
+    private router: Router,
+    public authService: AuthService,
+    public userService: Service
+  ) { 
+    this.user = {
+      photoURL: './assets/images/profile picture.jpg',
+    };
+    this.user.photoURL = './assets/images/profile picture.jpg';
+  }
 
   ngOnInit() {
-    const userId = this.route.snapshot.paramMap.get('id');
-    this.getUserData(userId);
   }
 
-  getUserData(userId: string) {
-    this.service.getUser(userId).subscribe((userData) => {
-      this.user = userData;
-      this.checkIfFriends(userId);
-    });
-  }
-
-  checkIfFriends(userId: string) {
-    const currentUserId = 'currentUserId'; // Replace 'currentUserId' with the ID of the currently logged-in user
-    this.areFriends = this.areUsersFriends(currentUserId, userId);
-  }
-
-  unfriend() {
-    const currentUserId = 'currentUserId'; // Replace 'currentUserId' with the ID of the currently logged-in user
-    this.service.unfriendUser(currentUserId, this.user.id).subscribe(
-      () => {
-        this.areFriends = false;
-      },
-      (error) => {
-        console.error('Error unfriending the user:', error);
-      }
-    );
-  }
-
-  areUsersFriends(currentUserId: string, otherUserId: string): boolean {
-    // Implement your logic to check if the users are friends and return the result
-    // For example, you could check if both users are in each other's friend list
-    return true; // Replace this with your actual logic
-  }
 }
