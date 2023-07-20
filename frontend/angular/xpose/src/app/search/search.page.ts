760ca63784f490a925d7e16d18226d18bcd15ba5
Mduduzi Sibiya <mdundoro@gmail.com>
2023-07-20 17:14:10 +0200

    Search pages updates

--- frontend/angular/xpose/src/app/search/search.page.ts
+++ frontend/angular/xpose/src/app/search/search.page.ts
@@ -1,21 +1,62 @@
-import { Component } from "@angular/core";
-import { ModalController } from "@ionic/angular";
+// search.page.ts
+import { Component, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IonSearchbar, NavController } from '@ionic/angular';
 
 @Component({
-  selector: "app-search",
-  templateUrl: "search.page.html",
-  styleUrls: ["search.page.scss"]
+  selector: 'app-search',
+  templateUrl: 'search.page.html',
+  styleUrls: ['search.page.scss'],
 })
 export class SearchPage {
-  searchQuery: string = "";
+  items = [
+    { name: 'Item 1' },
+    { name: 'Item 2' },
+    { name: 'Item 3' },
+    // Add more items as needed
+  ];
 
-  constructor(private modalController: ModalController) {}
+  filteredItems = this.items;
+  searchQuery = '';
 
-  dismissModal() {
-    this.modalController.dismiss();
+  @ViewChild('searchBar', { static: false }) searchBar: IonSearchbar | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private navCtrl: NavController
+  ) {}
+
+  ionViewDidEnter() {
+    this.route.queryParams.subscribe((params) => {
+      this.searchQuery = params['query'] || '';
+      this.onSearch();
+    });
   }
 
-  applySearch() {
-    this.modalController.dismiss({ searchQuery: this.searchQuery });
+  onSearch() {
+    const searchQuery = this.searchQuery.toLowerCase();
+
+    if (!searchQuery) {
+      this.filteredItems = this.items;
+      return;
+    }
+
+    this.filteredItems = this.items.filter((item) =>
+      item.name.toLowerCase().includes(searchQuery)
+    );
+  }
+
+  closeSearchBar() {
+    if (this.searchBar) {
+      this.searchBar.value = '';
+      this.searchBar.getInputElement().then((input: HTMLInputElement) => {
+        input.blur();
+      });
+    }
+  }
+
+  closeSearchPage() {
+    this.closeSearchBar();
+    this.navCtrl.pop();
   }
 }
