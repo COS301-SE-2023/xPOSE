b15e36a61efb56cb36a453786feec4fe3d1d3691
Sovaria Naidu <sovaria10@gmail.com>
2023-07-20 11:03:37 +0200

    user-profile component created

---
+++ frontend/angular/xpose/src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.scss'],
+})
+export class UserProfileComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
