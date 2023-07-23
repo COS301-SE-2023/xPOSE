7898d40c1606847def090f8cc8aabd27327ca201
Ibango7 <bangoisrael@gmail.com>
2023-07-23 14:57:34 +0200

    :construction: refactoring user-management-service

--- backend/user-management-service/controllers/sendFriendRequest.js
+++ backend/user-management-service/controllers/sendFriendRequest.js
@@ -1,6 +1,6 @@
-import User from '../DB/models/user.table.js';
-import Friend_request from '../DB/models/friend_request.table.js';
-import Friendship from '../DB/models/friendship.table.js';
+import User from '../data-access/models/user.table.js';
+import Friend_request from '../data-access/models/friend_request.table.js';
+import Friendship from '../data-access/models/friendship.table.js';
 import { sendMessageToQueue } from '../sender.js';
 
 export const sendFriendRequest = async (req, res) => {
