8830d760cd350accb893957ba704f36024787d51
Mduduzi Sibiya <mdundoro@gmail.com>
2023-07-24 09:32:44 +0200

    view event coverpage

--- frontend/angular/xpose/src/environments/environment.ts
+++ frontend/angular/xpose/src/environments/environment.ts
@@ -4,27 +4,27 @@
 
 export const environment = {
   firebase: {
-    projectId: "<SPOT 0>",
-    appId: "<SPOT 1>",
-    databaseURL: "<SPOT 2>",
-    storageBucket: "<SPOT 3>",
-    locationId: "<SPOT 4>",
-    apiKey: "<SPOT 5>",
-    authDomain: "<SPOT 6>",
-    messagingSenderId: "<SPOT 7>",
-    measurementId: "<SPOT 8>",
-    vapidKey: "<SPOT 9>"
+    projectId: "xpose-4f48c",
+    appId: "1:661170458893:web:114d0118efc5e071da2044",
+    databaseURL: "https://xpose-4f48c-default-rtdb.firebaseio.com",
+    storageBucket: "xpose-4f48c.appspot.com",
+    locationId: "us-central",
+    apiKey: "AIzaSyCErcy4MU2YpawjziirA1clGJiV3ZFD8-Y",
+    authDomain: "xpose-4f48c.firebaseapp.com",
+    messagingSenderId: "661170458893",
+    measurementId: "G-Y4N0R38032",
+    vapidKey: "BNcna4FuLcZQI1eoCx0mk9TEpu8m2hRWGEHEXh_0ww2XpRUqkihKkMsAGbkxD-JWXEYxfOtfQpJZ8VDCf82f378"
   },
-    googleMapsAPIKey: '<SPOT 10>',
     production: false
 };
-  /*
-   * For easier debugging in development mode, you can import the following file
-   * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
-   *
-   * This import should be commented out in production mode because it will have a negative impact
-   * on performance if an error is thrown.
-   */
-  // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
-  
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
 
