2910eeb3f32342fe08b1c6c9d0a5905673063de1
Mduduzi Sibiya <mdundoro@gmail.com>
2023-07-25 22:12:49 +0200

    footer

--- frontend/angular/xpose/src/app/footer/footer.component.ts
+++ frontend/angular/xpose/src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -7,8 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent  implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {}
+
+  onEvent(){
+		this.router.navigate(['/create-event']);
+	}
+	onNotifications(){
+		this.router.navigate(['/notification']);
+	}
+	onProfile(){
+		this.router.navigate(['/profile']);
+	}  
+	onJoinedEvent(){
+		this.router.navigate(['/joined-event']);
+	}
+	onHome(){
+		this.router.navigate(['/home']);
+	}
 
 }
