e844a32a2b789f9dab3696e182f514b2475349ef
Ibango7 <bangoisrael@gmail.com>
2023-07-27 00:58:08 +0200

    :construction: search user  Api functionaloty

--- backend/user-management-service/controllers/searchUser.js
+++ backend/user-management-service/controllers/searchUser.js
@@ -6,7 +6,31 @@ import { sendMessageToQueue } from '../sender.js';
 
 
 export const searchUser = async (req, res) => {
+    const {field, value} = req.query;
+
+    if(!field || !value){
+        return res.status(400).json({error: "Invalid parameters"});
+    }
+    
     const db = admin.firestore();
-    const usersCollectionRef = db.collection("Users")
+    const usersCollectionRef = db.collection("Users");
+
+    try{
+        const snapshot = await usersCollectionRef.where(field, "==",value).get();
+
+        if(snapshot.empty) {
+            return res.status(404).json({message: "No matching users found"});
+        }
+
+        const users = [];
+        snapshot.forEach((doc) => {
+            users.push({id: doc.id, ...doc.data()});
+        });
+
+        return res.status(200).json(users);
+    } catch(error) {
+        console.error("Error searching users:", error);
+        return res.status(5000).json({error: "Internal server error"});
+    }
 
 }
