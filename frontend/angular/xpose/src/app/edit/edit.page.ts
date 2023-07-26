05e8e83b609a33883a891c960b0b3b6fd4c4c8e9
Sovaria Naidu <sovaria10@gmail.com>
2023-07-26 17:11:30 +0200

    changes in edit.html

--- frontend/angular/xpose/src/app/edit/edit.page.ts
+++ frontend/angular/xpose/src/app/edit/edit.page.ts
@@ -2,7 +2,6 @@ import { AfterViewInit,Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from "../shared/services/auth.service";
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit',
