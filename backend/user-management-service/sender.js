f342bec5e785eb5ae6fb94a06d45187b9e362b49
Ibango7 <bangoisrael@gmail.com>
2023-07-27 00:31:24 +0200

    :construction: messageBuilder pattern for notifications

--- backend/user-management-service/sender.js
+++ backend/user-management-service/sender.js
@@ -1,45 +1,47 @@
-// import amqp from 'amqplib/callback_api.js';
-// import dotenv from 'dotenv';
-// dotenv.config();
-   
-// export function sendMessageToQueue(queue, msg) {
-//     const jsonString = JSON.stringify(msg);
-//     amqp.connect(process.env.CLOUDAMQP_URL, function (error0, connection) {
-//         if(error0) {
-//             throw error0;
-//         }
-    
-//         connection.createChannel(function (error1, channel) {
-//             if(error1) {
-//                 throw error1;
-//             }
-    
-//             channel.assertQueue(queue, {
-//                 durable: false
-//             });
-    
-//             channel.sendToQueue(queue, Buffer.from(jsonString));
-    
-//             console.log(`[x] Sent ${jsonString} to the ${queue} queue`);
-//         });
-        
-//         setTimeout(function () {
-//             connection.close();
-//             console.log('Connection closed...');
-//             // process.exit(0);
-//         }, 1000);
+import amqp from 'amqplib/callback_api.js';
+import dotenv from 'dotenv';
+import MessageBuilder from './controllers/messagebuilder';
 
-//     });
-// }
+dotenv.config();
+   
+export function sendMessageToQueue(queue, msg) {
+    const jsonString = JSON.stringify(msg);
+    amqp.connect(process.env.CLOUDAMQP_URL, function (error0, connection) {
+        if(error0) {
+            throw error0;
+        }
+    
+        connection.createChannel(function (error1, channel) {
+            if(error1) {
+                throw error1;
+            }
+    
+            channel.assertQueue(queue, {
+                durable: false
+            });
+    
+            channel.sendToQueue(queue, Buffer.from(jsonString));
+    
+            console.log(`[x] Sent ${jsonString} to the ${queue} queue`);
+        });
+        
+        setTimeout(function () {
+            connection.close();
+            console.log('Connection closed...');
+            // process.exit(0);
+        }, 1000);
+
+    });
+}
  
 
 
-// // const queueName = 'notifications';
+// const queueName = 'notifications';
 
-// // const message = new MessageBuilder()
-// // .setType("friend_request")
-// // .setMessage("Friend request from John Doe")
-// // .setSenderId("173")
-// // .setReceiverId("999")
-// // .build();
-// // sendMessageToQueue(queueName, message);
+// const message = new MessageBuilder()
+// .setType("friend_request")
+// .setMessage("Friend request from John Doe")
+// .setSenderId("173")
+// .setReceiverId("999")
+// .build();
+// sendMessageToQueue(queueName, message);
