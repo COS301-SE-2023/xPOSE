b16e2419a0d94e3672af9e3ce23534ce2b0a4c6a
Sovaria Naidu <sovaria10@gmail.com>
2023-07-27 16:12:34 +0200

    updated create-event.module.ts

--- frontend/angular/xpose/src/app/create-event/create-event.module.ts
+++ frontend/angular/xpose/src/app/create-event/create-event.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from "@ionic/angular";
 
 import { CreateEventPageRoutingModule } from "./create-event-routing.module";
@@ -13,6 +13,7 @@ import { SharedModule } from "../shared/shared.module";
     declarations: [CreateEventPage],
     imports: [
         CommonModule,
+        ReactiveFormsModule,
         FormsModule,
         IonicModule,
         CreateEventPageRoutingModule,
