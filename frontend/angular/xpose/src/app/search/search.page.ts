c7f1f8e78b236f9daabe2cd4bd9bdf2b514536f6
Ibango7 <bangoisrael@gmail.com>
2023-07-30 20:06:02 +0200

    :construction: plugging in search user endpoint

--- frontend/angular/xpose/src/app/search/search.page.ts
+++ frontend/angular/xpose/src/app/search/search.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-search',
@@ -15,18 +16,19 @@ export class SearchPage implements OnInit {
     { name: 'Event 3' },
     // Add more events as needed
   ];
-  users: any[] = [
-    { name: 'User 1' },
-    { name: 'User 2' },
-    { name: 'User 3' },
-    // Add more users as needed
-  ];
+
+  user: any  = {} // store user query result
+  loading: boolean = true; // Initial loading state
+  
   suggestedItems: any[] = [];
   searchQuery: string = '';
   filteredItems: any[] = [];
   showSuggestions: boolean = true;
+  searchIconClicked: boolean = false;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+    private http: HttpClient
+    ) {}
 
   ngOnInit() {
     // Initialize the filteredItems with all events on page load
@@ -42,17 +44,27 @@ export class SearchPage implements OnInit {
 
   onSearch() {
     if (this.searchQuery.trim() !== '') {
-      this.showSuggestions = false;
+      // this.showSuggestions = false;
       if (this.searchType === 'events') {
-        // Filter the events based on the searchQuery
-        this.filteredItems = this.events.filter(event =>
-          event.name.toLowerCase().includes(this.searchQuery.toLowerCase())
-        );
+
       } else if (this.searchType === 'users') {
-        // Filter the users based on the searchQuery
-        this.filteredItems = this.users.filter(user =>
-          user.name.toLowerCase().includes(this.searchQuery.toLowerCase())
-        );
+        console.log("Searched for " + this.searchQuery);
+        // http://localhost:8002/users/search?field=displayName&value=sov
+        const search_endpoint = `http://localhost:8000/u/users/search?field=displayName&value=${this.searchQuery}`;
+         this.loading = true;
+        this.http.get<any[]>(search_endpoint).subscribe(
+          (response) => {
+            this.user =response
+            this.loading = false;
+          }
+        )
+
+
+
+        // // Filter the users based on the searchQuery
+        // this.filteredItems = this.users.filter(user =>
+        //   user.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+        // );
       }
     } else {
       this.showSuggestions = true;
