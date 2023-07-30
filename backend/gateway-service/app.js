a36f9a1b1e460035576289dcee28cf0b1efa084d
Emito Khoza <emito.khoza@gmail.com>
2023-07-30 14:21:42 +0200

    Added testing

--- backend/gateway-service/app.js
+++ backend/gateway-service/app.js
@@ -2,25 +2,41 @@ const express = require('express');
 const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const routes = require('./routes.json');
+const axios = require('axios');
 
 const app = express();
 
 // Enable CORS
 app.use(cors());
 
+// Function to check if a target URL is online
+async function isTargetOnline(target) {
+  try {
+    await axios.head(target);
+    return 'Online';
+  } catch (error) {
+    return 'Offline';
+  }
+}
+
 // Create proxies for each route
-Object.entries(routes).forEach(([path, target]) => {
-  app.use(path, createProxyMiddleware({
+Object.entries(routes).forEach(async ([originalRoute, target]) => {
+  const path = `^${originalRoute}`;
+
+  app.use(originalRoute, createProxyMiddleware({
     target,
     pathRewrite: {
-      [`^${path}`]: '',
+      [path]: '',
     },
     changeOrigin: true,
   }));
+
+  const status = await isTargetOnline(target);
+  console.table([{ 'Original Route': originalRoute, 'Path Rewrite': '', 'Target': target, 'Status': status }]);
 });
 
-// Start the server
-const port = 8000;
-app.listen(port, () => {
-  console.log(`Gateway API server is running on port ${port}`);
+app.get('/', (req, res) => {
+    res.send('Gateway API server is running');
 });
+
+module.exports = app;
