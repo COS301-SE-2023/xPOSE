7b9a11ced295dce43d77ee547c252b11be70f869
Emito Khoza <emito.khoza@gmail.com>
2023-08-22 07:28:11 +0200

    Updated the settings page

--- frontend/angular/xpose/src/app/settings/settings.page.ts
+++ frontend/angular/xpose/src/app/settings/settings.page.ts
@@ -32,50 +32,10 @@ export class SettingsPage implements OnInit {
 	}
   }
 
-  async openImageGallery() {
-    const image = await Camera.getPhoto({
-      quality: 90,
-      allowEditing: true,
-      resultType: CameraResultType.Uri,
-      source: CameraSource.Prompt, // Allow the user to choose between the gallery and camera
-    });
-  
-    if (image && image.webPath) {
-      try {
-        // Convert image to Blob
-        const response = await fetch(image.webPath);
-        const blobImage = await response.blob();
-  
-        // Create FormData and append the image Blob to it
-        const formData = new FormData();
-        formData.append('image', blobImage, 'image.jpg');
-  
-        // Send the FormData to the server
-        // this.getCurrentUserId().subscribe((uid) => {
-        //   if (uid) {
-        //     this.http.post(`${this.api.apiUrl}/p/${this.current_event.code}?uid=${uid}`, formData).subscribe(
-        //       (res: any) => {
-        //         console.log(res);
-        //       },
-        //       (error: any) => {
-        //         console.error(error);
-        //       }
-        //     );
-        //   }
-        // });
-      } catch (error) {
-        console.error('Error while processing image:', error);
-      }
-    } else {
-      console.log("No image data available.");
-    }
-
-    // this.isGalleryOpen = true;
-  }
-  
-
   authorizeImage() {
     // Here you can implement the logic to authorize images
     // For now, let's assume images are authorized
   }
+
+  register() {}
 }
