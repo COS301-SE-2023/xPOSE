b0fb4c006ec77c53f4ee6c61e8cbb3d02a1d88ee
Mduduzi Sibiya <91707808+Duzzii@users.noreply.github.com>
2023-08-31 01:58:58 +0200

    the posts landing is working now

--- frontend/angular/xpose/src/app/event/event.page.ts
+++ frontend/angular/xpose/src/app/event/event.page.ts
@@ -34,11 +34,12 @@ interface Item {
   styleUrls: ['./event.page.scss'],
 })
 export class EventPage {
+  filterType: string = 'posts'; 
   event: Event;
   participants: any;
   private history: string[] = [];
   cards: any[] = []; // Array to store cards data
-  filterType: string = 'Ongoing';
+  // filterType: string = 'Ongoing';
   loading: boolean = true;
   errorMessage: string | undefined;
   events: any[] = []; // Array to store events data
