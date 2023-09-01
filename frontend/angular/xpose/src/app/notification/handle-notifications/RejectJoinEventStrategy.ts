2abf4884da35718f0cdca31d985505643857a7d6
Ibango7 <bangoisrael@gmail.com>
2023-09-01 03:32:09 +0200

    recycle: reject-Event-Request file structure

--- frontend/angular/xpose/src/app/notification/handle-notifications/RejectJoinEventStrategy.ts
+++ frontend/angular/xpose/src/app/notification/handle-notifications/RejectJoinEventStrategy.ts
@@ -1,27 +1,25 @@
-// import { NotificationRequestStrategy } from "./notificationRequestStrategy"
-// import { ApiService } from '../../service/api.service';
-// import { AuthService } from "src/app/shared/services/auth.service";
-// import { AngularFirestore } from "@angular/fire/compat/firestore";
-// import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { NotificationRequestStrategy } from "./notificationRequestStrategy"
+import { ApiService } from '../../service/api.service';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 
-// export class RejectJoinEventStrategy implements NotificationRequestStrategy{
-//     constructor(private api: ApiService,
-//         private http: HttpClient){
+export class RejectJoinEventStrategy implements NotificationRequestStrategy{
+    constructor(private api: ApiService,
+        private http: HttpClient){
 
-//     }
+    }
 
-//     async execute(user: any): Promise<void> {
-//         // const endpoint = `${this.api.apiUrl}/u/users/`;
-//         // const headers = new HttpHeaders().set('Content-Type', 'application/json');
-//         // const requestBody = JSON.stringify(user);
-//         //  try {
-//         //     const response = await this.http.post<any>(`${endpoint}${user.senderId}/friend-requests/${user.receiverId}/reject`, requestBody, {headers}).toPromise();
-//         //     console.log("Friend request rejected",response);
-//         //     // this.removeNotification(user);   
-//         //  } catch (error){
-//         //     console.error("Error:", error);
-//         //     throw error;
-//         //  }
-//     }
-// }
+    async execute(user: any): Promise<void> {
+        // const endpoint = `${this.api.apiUrl}/u/users/`;
+        // const headers = new HttpHeaders().set('Content-Type', 'application/json');
+        // const requestBody = JSON.stringify(user);
+        //  try {
+        //     const response = await this.http.post<any>(`${endpoint}${user.senderId}/friend-requests/${user.receiverId}/reject`, requestBody, {headers}).toPromise();
+        //     console.log("Friend request rejected",response);
+        //     // this.removeNotification(user);   
+        //  } catch (error){
+        //     console.error("Error:", error);
+        //     throw error;
+        //  }
+    }
+}
