8256009f7555d8699cdb7f194849962c58e5a243
Emito Khoza <emito.khoza@gmail.com>
2023-09-08 13:53:17 +0200

    Added readme for the posts service

--- backend/posts-service/index.py
+++ backend/posts-service/index.py
@@ -305,4 +305,4 @@ def health_check():
     return jsonify({'Message': 'Posts service is healthy'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
