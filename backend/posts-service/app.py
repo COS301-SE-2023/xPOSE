6a9ac8d3737e1a6bf81733aaab8d8410fe2a747c
Emito Khoza <emito.khoza@gmail.com>
2023-09-12 09:42:04 +0200

    Updated the posts app by adding post feature properly

--- backend/posts-service/app.py
+++ backend/posts-service/app.py
@@ -1,16 +1,121 @@
+# Include external libraries
+# import os
+# import sys
+# sys.path.append(os.path.join(os.path.dirname(__file__), 'libs'))
+# sys.path.append(os.path.join(os.path.dirname(__file__), 'data-access'))
+
 from flask import Flask, request, jsonify
-import os
-import sys
+from db_connector import *
+from firebase_functions import upload_image_to_firebase
 # face_recognition_functions.py is in /libs/
-sys.path.append(os.path.join(os.path.dirname(__file__), 'libs'))
 from face_recognition_functions import encode_and_store_face, decode_faces
+from google.cloud import firestore
+
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./permissions.json"
+
 
 app = Flask(__name__)
 
-@app.route('/:event_id', methods=['POST'])
-def create_post():
-    return jsonify({'Message': 'Created a post'}, 201)
+@app.route('/<event_id>', methods=['POST'])
+def create_post(event_id):
+    if request.method == 'POST':
+        try:
+            # Get user ID and image file from the request
+            user_id = request.form['user_id']
+            image_file = request.files['image']
 
+            # Check if the event exists in the Event table
+            try:
+                event = Event.get(Event.eid == event_id)
+            except Event.DoesNotExist:
+                # Event does not exist, create a new entry
+                event = Event.create(eid=event_id, is_encrypted=False, key=None)
+                # Check if event is in firestore in the Events collection
+                # TODO: In the event service, create an events collection
+                # firestore_client = firestore.Client()
+                # event_ref = firestore_client.collection('Events-Posts').document(event_id)
+                # event_doc = event_ref.get()
+                # if event_doc.exists:
+                #     # Event exists, create a new entry
+                #     event = Event.create(eid=event_id, is_encrypted=False, key=None)
+                # else:
+                #     return jsonify({'error': 'Event not found'}), 400
+
+            # Check if the user exists in the User table
+            try:
+                user = User.get(User.uid == user_id)
+            except User.DoesNotExist:
+                # return jsonify({'error': 'User not found'}), 400
+                # check if user is in firestore in the Users collection
+                firestore_client = firestore.Client()
+                user_ref = firestore_client.collection('Users').document(user_id)
+                user_doc = user_ref.get()
+                if user_doc.exists:
+                    # User exists, create a new entry
+                    user = User.create(uid=user_id, face_encoding=None)
+                else:
+                    return jsonify({'error': 'User not found'}), 400
+
+            print('user_id', user_id)
+            print('image_file', image_file.filename)
+
+            # Remove metadata from the image (if needed)
+            # Uncomment the line below if you want to remove metadata
+            # remove_metadata(image_file)
+
+            # Upload the image to Firebase Storage and get the image URL
+            image_url = upload_image_to_firebase(image_file)
+            print(image_url)
+
+            # Perform facial recognition to identify users in the image
+            result, status_code = decode_faces(image_file)
+            detected_users = result.get('detected_users', [])
+            print('Detected ', detected_users)
+
+            # Initialize Firestore client
+            firestore_client = firestore.Client()
+
+            # Create a new post document in the Firestore collection
+            posts_ref = firestore_client.collection(f'Event-Posts/{event_id}/posts')
+            new_post_doc = posts_ref.add({
+                'image_url': image_url,
+                'uid': user_id,
+                'detected_users': detected_users,
+                'timestamp': firestore.SERVER_TIMESTAMP
+            })
+
+            print(new_post_doc)
+
+            # Retrieve the generated document ID
+            # Retrieve the generated document ID from the tuple
+            post_doc_ref, post_doc_id = new_post_doc
+            print('Post Document ID:', post_doc_id.id)
+            print('Post Document Reference:', post_doc_ref)
+
+            post_id = post_doc_id.id
+
+
+            # Create a new post entry in the SQL 'posts' table
+            new_post = Post.create(
+                pid=post_id,
+                image_url=image_url,
+                event_eid=event_id,
+                post_owner_uid=user_id
+            )
+
+            # Add detected users to the PostTaggedUser table
+            for detected_user_id in detected_users:
+                PostTaggedUser.create(
+                    uid=detected_user_id,
+                    post_id=post_id
+                )
+
+            return jsonify({'message': 'Created a post', 'post_id': post_id}), 201
+
+        except Exception as e:
+            return jsonify({'error': str(e)}), 400
+        
 @app.route('/:event_id/:post_id', methods=['DELETE'])
 def delete_post():
     return jsonify({'Message': 'Deleted a post'}, 200)
@@ -35,7 +140,7 @@ def delete_like():
 def register_user():
     if request.method == 'POST':
         try:
-            user_id = request.form['user_id']
+            user_id = request.form['uid']
             image_file = request.files['image']
             result, status_code = encode_and_store_face(image_file, user_id)
             return jsonify(result), status_code
@@ -58,5 +163,6 @@ def health_check():
     return jsonify({'Message': 'Posts service is healthy'}, 200)
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    import rabbitmq_functions
+    create_tables()
+    app.run(debug=True)
+    # import rabbitmq_functions
