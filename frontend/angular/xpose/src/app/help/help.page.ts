e85f5aec38fb678738349b1a95ee41861bec9ce2
Sovaria Naidu <sovaria10@gmail.com>
2023-09-26 16:54:03 +0200

    restyled settings

--- frontend/angular/xpose/src/app/help/help.page.ts
+++ frontend/angular/xpose/src/app/help/help.page.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./help.page.scss'],
 })
 export class HelpPage implements OnInit {
+  currentPageName = 'Help';
 
   constructor() { }
 
