138eabfc43f41790ee8716a969b10ebe29ae9480
Emmanuella Birato <u19322594@tuks.co.za>
2023-09-27 21:09:41 +0200

    updated getEvents.test

--- backend/events-service/tests/getEvents.test.js
+++ backend/events-service/tests/getEvents.test.js
@@ -1,116 +1,67 @@
-const { getEvents } = require('../routes/getEvents');
+const { getEvents } = require('../routes/getEvents'); // Adjust the path accordingly
+const { User, Event, EventParticipant, EventJoinRequest, EventInvitation } = require('../data-access/sequelize');
 
-// Mock Sequelize models and Firebase Admin SDK
-jest.mock('../data-access/sequelize', () => {
-  const SequelizeMock = require('sequelize-mock');
-  const dbMock = new SequelizeMock();
+jest.mock('../data-access/sequelize', () => ({
+  User: {
+    findOne: jest.fn(),
+  },
+  Event: {
+    findAll: jest.fn(),
+  },
+  EventParticipant: {
+    findAll: jest.fn(),
+  },
+  EventJoinRequest: {
+    findOne: jest.fn(),
+  },
+  EventInvitation: {
+    findOne: jest.fn(),
+  },
+}));
 
-  const User = dbMock.define('User', {
-    uid: 'dgfydgsy5wf',
+const mockRequest = {
+  query: {
+    uid: 'mockUserId',
+    filter: 'all', 
+  },
+};
+
+const mockResponse = {
+  status: jest.fn().mockReturnThis(),
+  json: jest.fn(),
+};
+
+describe('getEvents', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
 
-  const Event = dbMock.define('Event', {
-    code: 'sftdtfy3fsf',
-    owner_id_fk: 'dgfydgsy5wf', 
-    start_date: '2023-08-01T12:00:00.000Z',
-    end_date: '2023-08-02T12:00:00.000Z',
-  });
+  it('should retrieve all events', async () => {
+    // Mock necessary functions
+    const mockUser = { id: 1 };
+    const mockEvent1 = {
+      id: 1,
+      start_date: '2023-09-27T10:00:00Z',
+      end_date: '2023-09-27T12:00:00Z',
+      owner: {
+        uid: 'ownerUid',
+      },
+    };
+    const mockEvent2 = {
+      id: 2,
+      start_date: '2023-09-28T10:00:00Z',
+      end_date: '2023-09-28T12:00:00Z',
+      owner: {
+        uid: 'ownerUid',
+      },
+    };
+    Event.findAll.mockResolvedValueOnce([mockEvent1, mockEvent2]);
+    User.findOne.mockResolvedValueOnce(mockUser);
+   
 
-  const EventParticipant = dbMock.define('EventParticipant', {
-    user_id_fk: 'dgfydgsy5wf',
-    event_id_fk: 'sftdtfy3fsf',
+    
   });
 
  
-
-  return {
-    sequelize: dbMock,
-    User,
-    Event,
-    EventParticipant,
-    
-  };
-});
-
-jest.mock('firebase-admin', () => ({
-  firestore: () => ({
-    collection: (collectionName) => ({
-      doc: (docId) => ({
-        get: jest.fn(() =>
-          Promise.resolve({
-            exists: docId === 'dgfydgsy5wf',
-            data: () => ({ displayName: 'John Doe' }),
-          })
-        ),
-      }),
-    }),
-  }),
-}));
-
-describe('getEvents', () => {
-  test('should get all events', async () => {
-    const req = {
-      query: {
-        uid: 'dgfydgsy5wf',
-      },
-    };
-    const res = {
-      json: jest.fn(),
-    };
-
-    await getEvents(req, res);
-
-    // Assertions
-    expect(res.json).toHaveBeenCalledWith([
-      
-      {
-        code: 'sftdtfy3fsf',
-        user_event_position: 'owner',
-        owner: 'dgfydgsy5wf',
-        
-      },
-    ]);
-  });
-
-  test('should get participant events', async () => {
-    const req = {
-      query: {
-        uid: 'dgfydgsy5wf',
-        filter: 'participant',
-      },
-    };
-    const res = {
-      json: jest.fn(),
-    };
-
-    await getEvents(req, res);
-
-    // Assertions
-    expect(res.json).toHaveBeenCalledWith([
-      
-      {
-        code: 'sftdtfy3fsf',
-        user_event_position: 'participant',
-        owner: 'dgfydgsy5wf',
-        
-      },
-    ]);
-  });
-
-  test('should return 400 when uid is missing', async () => {
-    const req = {
-      query: {},
-    };
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    };
-
-    await getEvents(req, res);
-
-    // Assertions
-    expect(res.status).toHaveBeenCalledWith(400);
-    expect(res.json).toHaveBeenCalledWith({ error: 'Missing required fields' });
-  });
 
 });
