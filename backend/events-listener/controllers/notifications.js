b9dd00ef1cafffcce6e20b3f3b57e8498d6130c9
Emito Khoza <emito.khoza@gmail.com>
2023-10-09 17:19:57 +0200

    Added the first listener code

---
+++ backend/events-listener/controllers/notifications.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import { receiveMessageFromQueue } from '../receiver.js';
+import admin from "firebase-admin";
+
+async function processReceivedMessage() {
+    try{
+        const queueName = 'notifications';
+        const receivedMsg = await receiveMessageFromQueue(queueName);
+        if(receivedMsg !== null){
+            console.log("Received message:", receivedMsg);
+            handleNotification(receivedMsg);
+        }else {
+            console.log("No messages received...");
+        }
+
+    } catch(error){
+        console.error("Error while receiving message", error);
+    }
+}
+
+processReceivedMessage();
+// Set up an interval to periodically check for new messages in the queue
+const intervalTime = 10000; // 5 seconds (adjust this as needed)
+setInterval(processReceivedMessage, intervalTime);
+
+export function handleNotification(message) {
+    console.log("Listening to incoming messages...")
+    
+    // Extract required data from the message
+    const {data } = message;
+    // Store the message in the Notification collection
+    const db = admin.firestore();
+    const notificationRef = db.collection('Notifications').doc(data.receiverId);
+    // Store the message in the MyNotifications subcollection
+    notificationRef.collection('MyNotifications').add({
+        ...data, notificationId:''})
+            .then((docRef)=> {
+                console.log("Notification sent successfully!");
+                
+                return docRef.update({
+                    notificationId: docRef.id
+                })
+           
+            })
+            .catch((error) => {
+                console.error("Error adding document: ", error);
+            });
+  }
+
+const service = express.Router();
+
+export default service;
